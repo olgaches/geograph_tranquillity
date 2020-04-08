45ffa2c7b8aba29bb19c9c8cb1f71d4997611ec2
olgaches <chessnokova@hotmail.com>
2020-04-08 16:12:35 +0200

    Update dependency.py

--- dependency.py
+++ dependency.py
@@ -1,48 +1,43 @@
-import os
 import pandas as pd
+import os
 import codecs
-import nltk
-from my_functions import replace_encoding_stuff
-from nltk.tokenize import word_tokenize, sent_tokenize
-# from nltk.parse.stanford import StanfordParser
-#
-# java_path = 'C:/Program Files (x86)/Java/jre1.8.0_221/bin/java.exe'
-# os.environ['JAVAHOME'] = java_path
-#
-# scp = StanfordParser(path_to_jar='C:/stanford-parser-full-2015-04-20/stanford-parser.jar',
-#                      path_to_models_jar='C:/stanford-parser-full-2015-04-20/stanford-parse-models-1.3.2.jar')
-
 import spacy
-import numpy
+from collections import Counter
 
+spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
-for token in doc:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-            token.shape_, token.is_alpha, token.is_stop)
+my_dirpath = '//files.geo.uzh.ch/private/ochesnok/home/Documents/2_projects/11_tranquillity/corpus/'
 
-#
-# my_dirpath = '//files.geo.uzh.ch/private/ochesnok/home/Documents/2_projects/11_tranquillity/corpus/'
-#
-# search_terms_updated = ['tranquillity','tranquility','tranquil','quiet','peaceful','calm']
-#
-# filename_input_text = 'all_tranquillity_descriptions_coord.csv'
-#
-# input_text_Geograph = pd.read_csv(os.path.join(my_dirpath, filename_input_text), delimiter=';;', encoding='latin1')
-# length = input_text_Geograph.shape[0]
+output_file = os.path.join(my_dirpath, 'all_tranquillity_dependencies_dates.csv')
+output_descriptions = codecs.open(output_file, 'w', 'utf-8')
+output_descriptions.writelines('gridimage_id;;comment;;serach_term;;dependency_word;;date;;year;;month;;\n')
 
-# for i in range(0, 1):
-#     comment = input_text_Geograph["comment"][i]
-#     try:
-#         gridimage_id = input_text_Geograph["gridimage_id"][i]
-#         comment = replace_encoding_stuff(str(comment))
-#         sent_tokenize_list = sent_tokenize(comment)
-#         for sentence in sent_tokenize_list:
-#
-#
-#
-#
-#
-#     except:
-#         print comment
+search_terms_updated = ['tranquillity','tranquility','tranquil','quiet','peaceful','calm']
+
+filename_input_text = 'all_tranquillity_descriptions_coord.csv'
+
+input_text_Geograph = pd.read_csv(os.path.join(my_dirpath, filename_input_text), delimiter=';;', encoding='latin1')
+length = input_text_Geograph.shape[0]
+
+dependency_words = []
+for i in range(0, length): 
+    comment = input_text_Geograph["comment"][i]
+    try:
+        gridimage_id = input_text_Geograph["gridimage_id"][i]
+        date = input_text_Geograph["image_taken"][i]
+        year = date[0:4]
+        month = date[5:7]
+        doc = nlp(comment)
+        for token in doc:
+            if token.dep_ == 'amod' and token.text in search_terms_updated:
+                #print(token.text, token.dep_, token.head.text)
+                #print(gridimage_id,token.text,token.head.text)
+                dependency_words.append(token.head.text)
+                output_descriptions.writelines(str(gridimage_id)+';;'+str(comment)+';;'+str(token.text)+';;'+str(token.head.text)+';;'+str(date)+';;'+str(year)+';;'+str(month)+'\n')
+    except:
+        print(comment)
+        
+set_result = ([[l, dependency_words.count(l)] for l in set(dependency_words)])
+
+print(sorted(set_result, key=lambda set_result: set_result[1], reverse = True)) 
